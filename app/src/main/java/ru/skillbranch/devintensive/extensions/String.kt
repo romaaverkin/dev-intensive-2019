1a6865ca2d14ab0257b6d5eb0c359d2d89ca8d42
Роман Аверкин <romaaverkin@gmail.com>
2019-07-09 12:55:19 +0700

    Задание 2 модуль 12.

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -4,3 +4,9 @@ fun String.truncate(len: Int = 16): String {
     val result = this.trim()
     return if (result.length <= len + 1) result else "${result.take(len).trimEnd()}..."
 }
+
+fun String.stripHtml(): String =
+    this
+        .replace("""<.*?>""".toRegex(), "")
+        .replace("""&(#\d+?|\w+?);""".toRegex(), "")
+        .split(""" +""".toRegex()).joinToString(" ")
