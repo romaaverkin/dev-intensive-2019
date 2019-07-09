3e339bea60e5fb2f4d7c32a5ffe29255640aa929
Роман Аверкин <romaaverkin@gmail.com>
2019-07-09 10:31:53 +0700

    Задание 2 модуль 3.

---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,22 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    fun parseFuLLName(fullName: String?): Pair<String?, String?> {
+        val parts: List<String>? = fullName?.trim()?.replaceAll("  ", " ")?.split(" ")
+
+        val firstName = if (parts?.getOrNull(0) == "") null else parts?.getOrNull(0)
+        val lastName = if (parts?.getOrNull(1) == "") null else parts?.getOrNull(1)
+
+        return firstName to lastName
+    }
+
+    private fun String.replaceAll(s: String, s1: String): String? {
+        var result = this
+
+        while (result.contains(s)) {
+            result = result.replace(s, s1)
+        }
+
+        return result
+    }
+}
