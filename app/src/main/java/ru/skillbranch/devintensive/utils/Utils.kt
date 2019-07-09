7ef58f1c0ec2cc78c2d52af38510433905242c6a
Роман Аверкин <romaaverkin@gmail.com>
2019-07-09 11:52:54 +0700

    Задание 2 модуль 7.

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,6 +1,13 @@
 package ru.skillbranch.devintensive.utils
 
 object Utils {
+    private val characterDictionaryMap = mapOf(
+        'а' to "a", 'б' to "b", 'в' to "v", 'г' to "g", 'д' to "d", 'е' to "e", 'ё' to "e", 'ж' to "zh", 'з' to "z",
+        'и' to "i", 'й' to "i", 'к' to "k", 'л' to "l", 'м' to "m", 'н' to "n", 'о' to "o", 'п' to "p", 'р' to "r",
+        'с' to "s", 'т' to "t", 'у' to "u", 'ф' to "f", 'х' to "h", 'ц' to "c", 'ч' to "ch", 'ш' to "sh", 'щ' to "sh",
+        'ъ' to "", 'ы' to "i", 'ь' to "", 'э' to "e", 'ю' to "yu", 'я' to "ya"
+    )
+
     fun parseFuLLName(fullName: String?): Pair<String?, String?> {
         val parts: List<String>? = fullName?.trim()?.replaceAll("  ", " ")?.split(" ")
 
@@ -32,5 +39,19 @@ object Utils {
         }
 
         return if (initialString != "") initialString else null
+    }
+
+    fun transliteration(payload: String, divider: String = " "): String {
+        var myString: String = ""
+
+        payload.forEach {
+            myString +=
+                when {
+                    it == ' ' -> divider
+                    it.isUpperCase() -> characterDictionaryMap[it.toLowerCase()]?.capitalize() ?: it.toString()
+                    else -> characterDictionaryMap[it] ?: it.toString()
+                }
+        }
+        return myString
     }
 }
