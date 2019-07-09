2aa58717362caa9545295e2db430da8a7bb74e54
Роман Аверкин <romaaverkin@gmail.com>
2019-07-09 14:57:25 +0700

    Исправил truncate

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -2,7 +2,7 @@ package ru.skillbranch.devintensive.extensions
 
 fun String.truncate(len: Int = 16): String {
     val result = this.trim()
-    return if (result.length <= len + 1) result else "${result.take(len + 1).trimEnd()}..."
+    return if (result.length <= len + 1) result else "${result.take(len).trimEnd()}..."
 }
 
 fun String.stripHtml(): String =
