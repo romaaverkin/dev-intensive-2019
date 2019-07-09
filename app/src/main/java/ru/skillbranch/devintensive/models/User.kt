10ee280f6524336193c5a00649942f7ecc02ac6f
Роман Аверкин <romaaverkin@gmail.com>
2019-07-09 14:42:52 +0700

    Исправил баги.

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -1,90 +1,92 @@
 package ru.skillbranch.devintensive.models
 
+import ru.skillbranch.devintensive.utils.Utils
 import java.util.*
-
-data class User(
-    val id: String,
-    var firstName: String?,
-    var lastName: String?,
-    var avatar: String?,
-    var rating: Int = 0,
-    var respect: Int = 0,
-    var lastVisit: Date? = Date(),
-    var isOnline: Boolean = false
-) {
-    constructor(id: String, firstName: String?, lastName: String?) : this(
+data class User (
+    val id : String,
+    var firstName : String?,
+    var lastName : String?,
+    var avatar : String?,
+    var rating : Int = 0,
+    var respect : Int = 0,
+    var lastVisit : Date? = Date(),
+    var isOnline : Boolean = false)
+{
+    constructor(id: String, firstName: String?, lastName: String?) : this (
         id = id,
         firstName = firstName,
         lastName = lastName,
         avatar = null
     )
 
-    companion object Factory {
-        private var lastId: Int = -1
-        fun makeUser(fullName: String?): User {
-            lastId++
-
-            val parts: List<String>? = fullName?.split(" ")
-
-            val firstName = parts?.getOrNull(0)
-            val lastName = parts?.getOrNull(1)
-
-            return User(id = "$lastId", firstName = firstName, lastName = lastName)
+    companion object Factory
+    {
+        fun makeUser(fullName: String) : User
+        {
+            val nameFragments : List<String> = fullName.split(" ")
+            return User(
+                UUID.randomUUID().toString(),
+                nameFragments.getOrNull(0) ?: "",
+                nameFragments.getOrNull(1) ?: ""
+            )
         }
     }
 
-    class Builder() {
-        var id: String = UUID.randomUUID().toString()
-        var firstName: String? = null
-        var lastName: String? = null
-        var avatar: String? = null
-        var rating: Int = 0
-        var respect: Int = 0
-        var lastVisit: Date? = Date()
-        var isOnline: Boolean = false
+    class Builder()
+    {
+        var id : String = UUID.randomUUID().toString()
+        var firstName : String? = null
+        var lastName : String? = null
+        var avatar : String? = null
+        var rating : Int = 0
+        var respect : Int = 0
+        var lastVisit : Date? = Date()
+        var isOnline : Boolean = false
 
-        fun id(id: String): Builder {
+        fun id(id: String): Builder
+        {
             this.id = id
             return this
         }
-
-        fun firstName(firstName: String?): Builder {
+        fun firstName(firstName: String?): Builder
+        {
             this.firstName = firstName
             return this
         }
-
-        fun lastName(lastName: String?): Builder {
+        fun lastName(lastName: String?): Builder
+        {
             this.lastName = lastName
             return this
         }
-
-        fun avatar(avatar: String?): Builder {
+        fun avatar(avatar: String?): Builder
+        {
             this.avatar = avatar
             return this
         }
-
-        fun rating(rating: Int): Builder {
+        fun rating(rating: Int): Builder
+        {
             this.rating = rating
             return this
         }
-
-        fun respect(respect: Int): Builder {
+        fun respect(respect: Int): Builder
+        {
             this.respect = respect
             return this
         }
-
-        fun lastVisit(lastVisit: Date?): Builder {
+        fun lastVisit(lastVisit: Date?): Builder
+        {
             this.lastVisit = lastVisit
             return this
         }
-
-        fun isOnline(isOnline: Boolean): Builder {
+        fun isOnline(isOnline: Boolean): Builder
+        {
             this.isOnline = isOnline
             return this
         }
-
-        fun build(): User {
+        fun build(): User
+        {
             return User(id, firstName, lastName, avatar, rating, respect, lastVisit, isOnline)
         }
+
     }
 }
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,57 +1,55 @@
 package ru.skillbranch.devintensive.utils
 
+import java.lang.IllegalStateException
+
 object Utils {
-    private val characterDictionaryMap = mapOf(
+
+    private val translitMap = mapOf(
         'а' to "a", 'б' to "b", 'в' to "v", 'г' to "g", 'д' to "d", 'е' to "e", 'ё' to "e", 'ж' to "zh", 'з' to "z",
         'и' to "i", 'й' to "i", 'к' to "k", 'л' to "l", 'м' to "m", 'н' to "n", 'о' to "o", 'п' to "p", 'р' to "r",
         'с' to "s", 'т' to "t", 'у' to "u", 'ф' to "f", 'х' to "h", 'ц' to "c", 'ч' to "ch", 'ш' to "sh", 'щ' to "sh",
         'ъ' to "", 'ы' to "i", 'ь' to "", 'э' to "e", 'ю' to "yu", 'я' to "ya"
     )
 
-    fun parseFuLLName(fullName: String?): Pair<String?, String?> {
-        val parts: List<String>? = fullName?.trim()?.replaceAll("  ", " ")?.split(" ")
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
+        val parts: List<String>? = fullName?.replaceAll("  ", " ")?.split(" ")
 
-        val firstName = if (parts?.getOrNull(0) == "") null else parts?.getOrNull(0)
-        val lastName = if (parts?.getOrNull(1) == "") null else parts?.getOrNull(1)
+        val firstName = parts?.notEmptyOrNullAt(0)
+        val lastName = parts?.notEmptyOrNullAt(1)
 
         return firstName to lastName
     }
 
-    private fun String.replaceAll(s: String, s1: String): String? {
+    private fun String.replaceAll(oldValue: String, newValue: String): String {
         var result = this
-
-        while (result.contains(s)) {
-            result = result.replace(s, s1)
+        while (result.contains(oldValue)) {
+            result = result.replace(oldValue, newValue)
         }
-
         return result
     }
 
-    fun toInitials(firstName: String?, lastName: String?): String? {
-        var initialString = ""
-
-        if (firstName != null && firstName.trim() != "") {
-            initialString += firstName[0].toUpperCase()
-        }
-
-        if (lastName != null && lastName.trim() != "") {
-            initialString += lastName[0].toUpperCase()
-        }
-
-        return if (initialString != "") initialString else null
+    private fun List<String>.notEmptyOrNullAt(index: Int) = getOrNull(index).let {
+        if ("" == it) null
+        else it
     }
 
-    fun transliteration(payload: String, divider: String = " "): String {
-        var myString: String = ""
-
+    fun transliteration(payload: String, divider: String = " ") = buildString {
         payload.forEach {
-            myString +=
+            append(
                 when {
                     it == ' ' -> divider
-                    it.isUpperCase() -> characterDictionaryMap[it.toLowerCase()]?.capitalize() ?: it.toString()
-                    else -> characterDictionaryMap[it] ?: it.toString()
+                    it.isUpperCase() -> translitMap[it.toLowerCase()]?.capitalize() ?: it.toString()
+                    else -> translitMap[it] ?: it.toString()
                 }
+            )
         }
-        return myString
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? = when {
+        firstName.isNullOrBlank() && lastName.isNullOrBlank() -> null
+        !firstName.isNullOrBlank() && lastName.isNullOrBlank() -> firstName[0].toUpperCase().toString()
+        firstName.isNullOrBlank() && !lastName.isNullOrBlank() -> lastName[0].toUpperCase().toString()
+        !firstName.isNullOrBlank() && !lastName.isNullOrBlank() -> firstName[0].toUpperCase() + lastName[0].toUpperCase().toString()
+        else -> throw IllegalStateException("Incorrect state in 'when' expression")
     }
 }
